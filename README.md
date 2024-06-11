4cabd03439f48773501bbb89ff7939d6a3e99292
Emmanuel Jatto <emmanueljatto@Emmanuels-Air.lan>
2024-06-10 22:21:11 -0400

    created weather cards, and made changes to readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # weather-dashboard
+The goal of this project was to create a weather dashboard, so that a user could get the weather of a city.  The website needed to take a user input and be able to store it in local storage and display the previous searches on the left of the screen.  The page also needed to be able to use the Weather API and day.js to be able to get the weather and current date to display on the page.  
+
+The biggest challenge I faced when creating this website was, figuring out how to access the information given in the api and display it on the screen.  I solved this by calling certain functions in the api response call.  
+
+![Image of Website]()
+[Weather Dashboard Deployable Link]()
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const apiKey = '23aea221ad655ccd9dd94de4e15df7fb';
 const searchBox = $('#search-list');
-
+const weatherBox = $('#weather-card');
 
 function getLocation(cityName) {
     const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
@@ -12,6 +12,8 @@ function getLocation(cityName) {
         const lon = data[0].lon;
         getWeather(lat,lon);
     })
+
+
 }
 
 function getWeather(lat,lon) {
@@ -21,7 +23,51 @@ function getWeather(lat,lon) {
         return response.json();
     }).then(function(data) {
         console.log(data);
+        createWeatherCard(data);
     })
+}
+
+function createWeatherCard(data) {
+    weatherBox.empty();
+
+    const fahrenheitTemp = ((data.list[0].main.temp - 273.15) * 1.8) + 32
+    const roundedTemp = fahrenheitTemp.toFixed(2);
+
+    const cityName = $('#city-search').val();
+    const currentDate = dayjs().format('M/DD/YYYY');
+
+    const weatherCard = $('<div>');
+    weatherCard.addClass('card task-card my-3');
+    const weatherCardHeader = $('<div>').addClass('card-header h4').text(`${cityName} ${currentDate}`);
+    const weatherCardBody = $('<div>').addClass('card-body');
+    const weatherCardText = $('<p>').addClass('card-text').text(`Temp: ${roundedTemp}°F`);
+    const weatherCardText2 = $('<p>').addClass('card-text').text(`Wind: ${data.list[0].wind.speed} MPH`);
+    const weatherCardText3 = $('<p>').addClass('card-text').text(`Humidity: ${data.list[0].main.humidity} %`)
+
+    $('#weather-card').append(weatherCard);
+    weatherCard.append(weatherCardHeader);
+    weatherCard.append(weatherCardBody);
+    weatherCardBody.append(weatherCardText);
+    weatherCardBody.append(weatherCardText2);
+    weatherCardBody.append(weatherCardText3);
+
+
+    for(let i = 1; i <= data.list.length; (i += 8)) {
+        const forecastCard = $('<div>');
+        forecastCard.addClass('card task-card my-3 col-sm-3');
+        const forecastCardHeader = $('<div>').addClass('card-header h6').text(`${data.list[i].dt_txt}`);
+        const forecastBody = $('<div>').addClass('card-body');
+        const forecastText = $('<p>').addClass('card-text').text(`Temp: ${(((data.list[i].main.temp - 273.15) * 1.8) + 32).toFixed(2)}°F`);
+        const forecastText2 = $('<p>').addClass('card-text').text(`Wind: ${data.list[i].wind.speed} MPH`);
+        const forecastText3 = $('<p>').addClass('card-text').text(`Humidity: ${data.list[i].main.humidity} %`)
+
+        $('#forcast').append(forecastCard);
+        forecastCard.append(forecastCardHeader);
+        forecastCard.append(forecastBody);
+        forecastBody.append(forecastText);
+        forecastBody.append(forecastText2);
+        forecastBody.append(forecastText3);
+    }
 }
 
 function HandleSubmit(event) {
@@ -31,15 +77,12 @@ function HandleSubmit(event) {
     const cityName = $('#city-search').val();
 
     citySearches.push(cityName);
-    localStorage.setItem('cityName', JSON.stringify(citySearches))
+    localStorage.setItem('cityName', JSON.stringify(citySearches));
+    getLocation(cityName);
     renderCityList();
 }
 
 function createCityList(cityName) {
-    // const cityNameBox = $('<div>')
-    // searchBox.append(cityNameBox);
-    // searchBox.text(cityName);
-    // console.log(cityName);
 
     let citySearches = JSON.parse(localStorage.getItem('cityName')) || [];
     
@@ -59,12 +102,6 @@ function renderCityList() {
     let citySearches = JSON.parse(localStorage.getItem('cityName')) || [];
 
     createCityList();
-    // citySearches.forEach(function(cityName) {
-    //     createCityList(cityName);
-    // });
-    // for(let i = 0; i < citySearches.length; i++) {
-    //     createCityList(citySearches[i]);
-    // }
 }
 
 $(document).ready(function () {
@@ -72,5 +109,3 @@ $(document).ready(function () {
 
     $('#submit').on('click', HandleSubmit);
 });
-
-// getLocation('Paris');
